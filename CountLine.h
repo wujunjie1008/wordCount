96167c4e0b0fe5f063251c01897e3b34e677ac16
wujunjie1008 <445404396@qq.com>
2019-10-19 17:35:15 +0800

    031702609
    计算行数

---
+++ CountLine.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+  
+#include <stdlib.h>
+using namespace std;
+int countline(string stri)
+{
+int line = 0;
+int i=0;
+for(i=0;i<stri.length();i++)
+{
+	if(stri[i] == '\n'&&stri[i-1] != '\n')
+	line ++;
+}
+if(stri[i-1] != '\n')
+line ++;
+return line;
+}
