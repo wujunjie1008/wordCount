ac5f0e959865d59c50898b1a72af9ff65a917a80
Brokenpumpkin <9606465519@qq.com>
2019-10-19 11:32:44 +0800

    output_1st

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
-  
+#include<string.h>  
 #include <stdlib.h>
 using namespace std;
 struct str{
@@ -32,14 +32,43 @@ buf.put(ch);
 //返回与流对象buf关联的字符串
 return buf.str();
 }
-  
+
+ void outputRes(char *output)
+{
+	char *data;
+	data=output;
+	char way[]="./";
+	int charactersNumber,wordsNumb,linesNumb,showTimeLines;
+	double showTime;
+	string name="monday tuesday wednesday";
+	charactersNumber=2;
+	wordsNumb=1;
+	linesNumb=1;
+	showTimeLines=2;
+	showTime=3;	
+	strcat(way,output);
+	cout<<way; 
+	ofstream outfile;
+	outfile.open(way);
+	outfile<<"characters: "<<charactersNumber<<endl;
+	outfile<<"words: "<<wordsNumb<<endl;
+	outfile<<"lines: "<<linesNumb<<endl;
+	
+	for(int i=0;i<showTimeLines;i++)
+	{
+		outfile<<"<"<<name<<">: "<<showTime<<endl;
+	}
+	outfile.close();
+}
 int main()
 {
-//文件名
-char * fn="input.txt";
-string str;
-str=readFileIntoString(fn);
-printf("%c",str[12]);
+	//文件名
+	char fn[]="input.txt";
+	char output[]="output.txt";
+	
+	string str;
+	str=readFileIntoString(fn);
+	outputRes(output);
   
 }
 
